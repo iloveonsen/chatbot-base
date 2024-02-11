025140d5fb4c8dd6b510791fab29bf44672f4efc
황재훈_T6193 <jayhwang.cloud@gmail.com>
2024-02-12 02:40:44 +0900

    feat - add session models and page, chat input form processing (#10)
    #9

--- chat/models.py
+++ chat/models.py
@@ -1,3 +1,49 @@
 from django.db import models
+from django.contrib.auth.models import User
+from users.models import Profile
+import uuid
 
 # Create your models here.
+
+class ChatSession(models.Model):
+    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
+    title = models.CharField(max_length=100, blank=True, null=True)
+    owner = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='chat_sessions')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'{self.id} - {self.owner.name}'
+    
+    class Meta:
+        ordering = ['-created_at']
+    
+
+class UserMessage(models.Model):
+    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
+    session = models.ForeignKey(ChatSession, on_delete=models.CASCADE, related_name='user_messages')
+    owner = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    message = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.id} - {self.owner.name} - {self.session.id}'
+    
+    class Meta:
+        ordering = ['created_at']
+
+
+class BotResponse(models.Model):
+    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
+    session = models.ForeignKey(ChatSession, on_delete=models.CASCADE, related_name='bot_responses')
+    user_message = models.OneToOneField(UserMessage, on_delete=models.CASCADE, related_name='bot_response')
+    response = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.id} - {self.session.title}'
+    
+    class Meta:
+        ordering = ['created_at']
+    
+
