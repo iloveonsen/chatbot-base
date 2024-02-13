e67c8911206e03c1b3801b3d2ede4322d4baa20c
DonghaeSuh <gypsi12@naver.com>
2024-02-13 20:15:20 +0900

    feat - add langchain rag system
    #15

--- chat/utils.py
+++ chat/utils.py
@@ -4,12 +4,27 @@ from langchain.chains import LLMChain, ConversationChain
 from langchain_core.prompts import ChatPromptTemplate
 from langchain.schema import HumanMessage, SystemMessage, AIMessage
 from langchain.memory import ConversationBufferMemory
+import faiss
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain_openai import OpenAI
+from langchain.docstore import InMemoryDocstore
+from langchain.vectorstores import FAISS
+from langchain.memory import VectorStoreRetrieverMemory
 
 from chat.models import UserMessage, BotResponse
 
 from dotenv import load_dotenv
 
 load_dotenv()
+
+embedding_size = 1536 # Dimensions of the OpenAIEmbeddings
+index = faiss.IndexFlatL2(embedding_size)
+
+embedding_fn = OpenAIEmbeddings()
+vectorstore = FAISS(embedding_fn, index, InMemoryDocstore({}), {})
+retriever = vectorstore.as_retriever(search_kwargs=dict(k=5))
+
+memory = VectorStoreRetrieverMemory(retriever=retriever,return_docs=True)
 
 def summarize_session_title(user_message):
     llm = OpenAI(temperature=0)
@@ -27,27 +42,39 @@ def summarize_session_title(user_message):
 
 
 def get_chatbot_response(user_message, session, owner):
-    chat = ChatOpenAI(temperature=.2)
 
-    messages = [
-        SystemMessage(
-            content="You are a helpful ai kindly reply to user."
-        ),
-    ]
+    llm = OpenAI(temperature=0) 
 
-    existing_user_messages = UserMessage.objects.filter(session=session, owner=owner)
-    for existing_user_message in existing_user_messages:
-        messages.append(HumanMessage(content=existing_user_message.message))
-        try:
-            existing_bot_response = BotResponse.objects.get(user_message=existing_user_message)
-        except Exception as e:
-            break
-        messages.append(AIMessage(content=existing_bot_response.response))
-    
-    messages.append(HumanMessage(content=user_message))
 
-    response = chat.invoke(messages)
-    return response.content.strip()
+    _DEFAULT_TEMPLATE = """다음은 인간과 인공지능 간의 매우 친근한 대화입니다. AI는 맥락에서 많은 구체적인 세부 정보를 제공합니다. AI가 질문에 대답할 수 없으면 솔직하게 그렇다고 말합니다.
+
+    현재 대화와 관련된 이전 대화 정보:
+    {history}
+
+    (관련이 없는 이전 대화 정보는 사용할 필요가 없습니다.)
+
+    (AI는 이전 대화 정보 중에서 인간이 질문한 내용과 관련된 정보를 이해하여 친근하게 답변합니다.)
+    현재 대화:
+    인간: {input}
+    AI:"""
+
+
+    PROMPT = PromptTemplate(
+        input_variables=["history", "input"], template=_DEFAULT_TEMPLATE
+    )
+
+
+    conversation_with_summary = ConversationChain(
+        llm=llm,
+        prompt=PROMPT,
+        memory=memory,
+        verbose=True
+    )
+
+    response = conversation_with_summary.predict(input=user_message)
+
+
+    return response.strip()
 
 
 # if __name__ == "__main__":
