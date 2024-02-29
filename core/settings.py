c1356a57a1105e6d189c2b624ad51c7af352b3be
iloveonsen <jayhwang.cloud@gmail.com>
2024-02-29 21:39:58 +0900

    feat - add postgresql settings
    #22

--- core/settings.py
+++ core/settings.py
@@ -136,8 +136,12 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'djangotest',
+        'USER': 'testuser',
+        'PASSWORD': 'testpassword',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
